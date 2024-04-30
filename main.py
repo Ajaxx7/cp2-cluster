8dc6d6433dbf39ac5acd5d92b17be2eefec68f03
Ajaxx7 - Gustavo Enrique Galvão <89093783+Ajaxx7@users.noreply.github.com>
2024-04-29 22:29:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from typing import Union
+def main():
+  print("Hello World")
 
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+  main()
